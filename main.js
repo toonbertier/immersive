9505d00ca51c253ecc1014bbe076fdc9e8740087
Toon Bertier <toonbertier@gmail.com>
2015-11-02 16:08:55 +0100

    changed folder structure

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ let BrowserWindow = require('browser-window');
 let five = require('johnny-five');
 let board = five.Board();
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
 
 app.on('window-all-closed', function() {
@@ -19,34 +17,33 @@ app.on('window-all-closed', function() {
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
 app.on('ready', function() {
+
   mainWindow = new BrowserWindow({width: 1440, height: 720});
-
   mainWindow.openDevTools();
-
-  board.on('ready', function() {
-    let potentiometer = new five.Sensor({
-       pin: "A0",
-       freq: 250
-    });
-
-    board.repl.inject({
-      pot: potentiometer
-    });
-
-    potentiometer.on("data", function() {
-      let value = this.raw;
-      mainWindow.webContents.send('move', value);
-    });
-  });
-
-  // load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+
+  board.on('ready', initBoard);
 
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
+
 });
+
+const initBoard = () => {
+  let potentiometer = new five.Sensor({
+     pin: "A0",
+     freq: 250
+  });
+
+  board.repl.inject({
+    pot: potentiometer
+  });
+
+  potentiometer.on("data", function() {
+    let value = this.raw;
+    mainWindow.webContents.send('move', value);
+  });
+}
 
