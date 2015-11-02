e6cc311ca66d0990f6941eed884215779ab98e9b
Toon Bertier <toonbertier@gmail.com>
2015-11-02 19:33:04 +0100

    infinite moving stars

--- js/mainWindowRenderer.js
+++ js/mainWindowRenderer.js
@@ -2,49 +2,69 @@
 
 let ipc = require('ipc');
 
-// ThreeJS
+let spheres = [];
 
-let cameraPos;
 let effect;
-
-let camera = new THREE.PerspectiveCamera(60, 720/480 , 1, 100000);
+let camera = new THREE.PerspectiveCamera(90, 720/480 , 1, 100000);
 let scene = new THREE.Scene();
 
 let renderer = new THREE.WebGLRenderer();
 renderer.setSize( 720, 480 );
 document.body.appendChild(renderer.domElement);
 
-let geometry = new THREE.BoxGeometry(1, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0xffffff });
-let cube1 = new THREE.Mesh(geometry, material);
-
-cube1.rotation.x = 10;
-cube1.rotation.y = 30;
-cube1.position.z = 0.05;
-cube1.position.x = -3;
-scene.add(cube1);
-
-let cube2 = new THREE.Mesh(geometry, material);
-
-cube2.rotation.x = 20;
-cube2.rotation.y = 50;
-cube2.position.z = 1.6;
-cube2.position.x = 1;
-scene.add(cube2);
+let geometry = new THREE.SphereGeometry(2, 32, 32);
+let material = new THREE.MeshBasicMaterial({ color: 0xdddddd, wireframe: true });
 
 effect = new THREE.AnaglyphEffect(renderer);
 effect.setSize( 720, 480 );
 
-camera.position.z = 5;
+camera.position.z = 200;
 
-ipc.on('move', function(arg){
-	cameraPos = arg/100;
-})
+// ipc.on('move', function(arg){
+// 	cameraX = arg/1000;
+// 	cameraZ = 5 - arg/1000;
+// });
+
+const removeOutOfBounds = (sphere) => {
+	if(sphere.position.z > 200) {
+		scene.remove(sphere);
+		spheres.remove(spheres.findIndex(s => s.uuid == sphere.uuid));
+		return true;
+	}
+	return false;
+}
+
+const createNewSphere = () => {
+	let sphere = new THREE.Mesh(geometry, material);
+	sphere.position.z = Math.random() * 50;
+	sphere.position.x = Math.random() * 300 - 150;
+	sphere.position.y = Math.random() * 200 - 100;
+	scene.add(sphere);
+
+	return sphere;
+}
 
 const render = () => {
 	requestAnimationFrame(render);
-	camera.position.x = cameraPos;
 	effect.render(scene, camera);
+
+	spheres.forEach(sphere => {
+		sphere.position.z += 0.3;
+		if(removeOutOfBounds(sphere)){
+			spheres.push(createNewSphere());
+		}
+	});
 }
 
-render();
+const init = () => {
+
+	for(let i = 0; i < 50; i++) {
+		spheres.push(createNewSphere());
+	}
+
+	console.log(spheres);
+
+	render();
+}
+
+init();
